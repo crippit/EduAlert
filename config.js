932e6895a7bda7cf93848ffd2902a49e3ac7b33c
crippit <crippit@gmail.com>
2022-03-04 12:38:25 -0500

    Create config.js

---
+++ config.js
@@ -0,0 +1,4 @@
+window.GoogleSamples = window.GoogleSamples || {};
+window.GoogleSamples.Config = window.GoogleSamples.Config || {
+  gcmAPIKey: 'AAAArXOW9sI:APA91bEBaR1qa9fLWzFWXuGrMf0VrAMSp8qCArQ0_Aj7aPfxdf0dAC1imal0Fo4Wpod9PZp4ARQML3OZWRqsnfEN1IWJXFnua'
+};
